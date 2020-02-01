eb6970b0d1eae49cccdb28ec4af297c90a01a202
Cyrille leRolland <le_rolland@yahoo.fr>
2020-02-01 11:50:26 +0100

    ajout inutile  pour cloturer la barche

--- config/routes.rb
+++ config/routes.rb
@@ -2,20 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
 
-  get "campaings", to: "campaigns#index"
-  get "campaings/:id", to: "campaigns#show"
-  get "campaigns/new", to: "campaigns#new"
-  post "campaigns", to: "campaigns#create"
-
-  get "users", to: "users#index"
-  get "users/:id", to: "users#show"
-  get "users/new", to: "users#new"
-  post "users", to: "users#create"
-  get "users/:id/edit", to: "users#edit"
-  patch "users/:id", to: "users#update"
-  delete "users/:id", to: "users#destroy"
-
-
   resources :campaigns, only: [:index, :new, :create, :show]
   resources :users
 end
