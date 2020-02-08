c7150eed4f46a7d11683e2939d81f63a3b81b3b3
Cyrille leRolland <le_rolland@yahoo.fr>
2020-02-08 14:32:12 +0100

    hashtag

---
+++ app/models/hashtag.rb
@@ -0,0 +1,3 @@
+class Hashtag < ApplicationRecord
+  belongs_to :campaign
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_04_192524) do
+ActiveRecord::Schema.define(version: 2020_02_08_110945) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,12 @@ ActiveRecord::Schema.define(version: 2020_02_04_192524) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_campaigns_on_user_id"
+  end
+
+  create_table "hashtags", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "influencer_tags", force: :cascade do |t|
