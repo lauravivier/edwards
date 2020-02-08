acb3aa0099710f7bf93f67cd2d712384a1bf6366
ken wang <wang.ken09@gmail.com>
2020-02-08 18:09:06 +0100

    search

--- app/controllers/campaigns_controller.rb
+++ app/controllers/campaigns_controller.rb
@@ -15,6 +15,17 @@ class CampaignsController < ApplicationController
     @influencers = Influencer.all
   end
 
+  def step_2
+    @influencers = Influencer.all
+      if params[:query].present?
+      sql_query = " \
+      influencers.community_location ILIKE :query \
+      "
+      @influencers = Influencer.joins(:tag).where(sql_query, query: "%#{params[:query]}%")
+    else
+      @influencers = []
+  end
+
   def edit
     @campaign = Campaign.find(params[:id])
   end
--- app/controllers/influencers_controller.rb
+++ app/controllers/influencers_controller.rb
@@ -2,16 +2,11 @@ class InfluencersController < ApplicationController
   def index
     if params[:query].present?
       sql_query = " \
-      influencers.name ILIKE :query \
-      OR influencers.community_location ILIKE :query \
-      OR influencers.community_age ILIKE :query \
-      OR influencers.engagement_rate ILIKE :query \
-      OR influencers.media ILIKE :query \
-      OR tags.name ILIKE :query \
+      influencers.community_location ILIKE :query \
       "
       @influencers = Influencer.joins(:tag).where(sql_query, query: "%#{params[:query]}%")
     else
-      @influencers = Influencer.all
+      @influencers = []
     end
   end
 end
