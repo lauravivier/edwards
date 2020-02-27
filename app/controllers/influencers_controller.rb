b60a3d174a65ed9bd332f8f380df7b58455132de
ken wang <wang.ken09@gmail.com>
2020-02-27 07:21:29 +0100

    modifs cards et button

--- app/controllers/influencers_controller.rb
+++ app/controllers/influencers_controller.rb
@@ -2,8 +2,8 @@ class InfluencersController < ApplicationController
 
     def index
       if params["query"]
-        @filter = params["query"]["ages"].concat(params["query"]["medias"]).concat(params["query"]["sizes"]).concat(params["query"]["locations"]).flatten.reject(&:blank?)
-        @influencers = Influencer.all.global_search("#{@filter}").order(name: :asc)
+        @filtrer = params["query"]["ages"].concat(params["query"]["medias"]).concat(params["query"]["sizes"]).concat(params["query"]["locations"]).flatten.reject(&:blank?)
+        @influencers = Influencer.all.global_search("#{@filtrer}").order(name: :asc)
       else
         @influencers = Influencer.all.order(name: :asc)
       end
--- db/migrate/20200204080230_create_influencers.rb
+++ db/migrate/20200204080230_create_influencers.rb
@@ -2,9 +2,11 @@ class CreateInfluencers < ActiveRecord::Migration[6.0]
   def change
     create_table :influencers do |t|
       t.string :name
-      t.integer :women_stats
-      t.integer :men_stats
+      t.integer :followers
+      t.integer :posts
       t.string :engagement_rate
+      t.string :influencer_type
+      t.integer :likes
       t.timestamps
     end
   end
